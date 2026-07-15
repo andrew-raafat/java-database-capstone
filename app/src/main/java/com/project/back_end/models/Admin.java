8ac35f9bf490999357f9cb2dd7587ebac7934560
andrew-raafat <andrewraafat35@yahoo.com>
2026-07-15 14:48:09 +0300

    Completed model classes for Clinic Management System

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -1,6 +1,45 @@
 package com.project.back_end.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Entity
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    public Admin() {}
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 
 // @Entity annotation:
 //    - Marks the class as a JPA entity, which means it represents a table in the database.
--- app/src/main/java/com/project/back_end/models/Appointment.java
+++ app/src/main/java/com/project/back_end/models/Appointment.java
@@ -1,8 +1,101 @@
 package com.project.back_end.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
 public class Appointment {
 
-  // @Entity annotation:
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    private Doctor doctor;
+
+    @NotNull
+    @ManyToOne
+    private Patient patient;
+
+    @Future
+    private LocalDateTime appointmentTime;
+
+    @NotNull
+    private int status;
+
+    @Transient
+    private LocalDateTime getEndTime(){
+        return appointmentTime.plusHours(1);
+    }
+
+    @Transient
+    private LocalDate getAppointmentDate(){
+        return appointmentTime.toLocalDate();
+    }
+
+    @Transient
+    private LocalTime getAppointmentTimeOnly(){
+        return appointmentTime.toLocalTime();
+    }
+
+    public Appointment() {
+    }
+
+    public Appointment(Long id, Doctor doctor, Patient patient, LocalDateTime appointmentTime, int status) {
+        this.id = id;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    // @Entity annotation:
 //    - Marks the class as a JPA entity, meaning it represents a table in the database.
 //    - Required for persistence frameworks (e.g., Hibernate) to map the class to a database table.
 
